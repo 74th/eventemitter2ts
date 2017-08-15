6d7b57226ddda62d642388848482dba4fefe28ed
74th <site@j74th.com>
2017-08-15 17:26:50 +0900

    up

--- eventemitter2.d.ts
+++ eventemitter2.d.ts
@@ -29,29 +29,29 @@ export interface ConstructorOptions {
 export interface Listener<T> {
 	(arg: T): any;
 }
-export interface EventAndListener<K> {
-	(event: K | K[], ...values: any[]): void;
+export interface EventAndListener<K, T> {
+	(event: K | K[], arg: T): void;
 }
 
 export declare class EventEmitter2<T> {
 	constructor(options?: ConstructorOptions)
 	emit<K extends keyof T>(event: K | K[], arg: T[K]): boolean;
-	emitAsync<K extends keyof T>(event: K | K[], arg: T[K]): Promise<any[]>;
-	addListener<K extends keyof T>(event: K, listener: (arg: T[K]) => any): this;
+	emitAsync<K extends keyof T>(event: K | K[], arg: T[K]): Promise<T[K]>;
+	addListener<K extends keyof T>(event: K, listener: Listener<T[K]>): this;
 	on<K extends keyof T>(event: K | K[], listener: Listener<T[K]>): this;
-	prependListener<K extends keyof T>(event: K | K[], listener: (arg: T[K]) => any): this;
+	prependListener<K extends keyof T>(event: K | K[], listener: Listener<T[K]>): this;
 	once<K extends keyof T>(event: K | K[], listener: Listener<T[K]>): this;
 	prependOnceListener<K extends keyof T>(event: string | string[], listener: Listener<T[K]>): this;
-	many<K extends keyof T>(event: string | string[], timesToListen: number, listener: Listener<T[K]>): this;
-	prependMany<K extends keyof T>(event: string | string[], timesToListen: number, listener: Listener<T[K]>): this;
-	// onAny<K extends keyof T>(listener: EventAndListener<T[K]>): this;
-	// prependAny<K extends keyof T>(listener: EventAndListener<T[K]>): this;
+	many<K extends keyof T>(event: K | K[], timesToListen: number, listener: Listener<T[K]>): this;
+	prependMany<K extends keyof T>(event: K | K[], timesToListen: number, listener: Listener<T[K]>): this;
+	onAny<K extends keyof T>(listener: EventAndListener<K, T[K]>): this;
+	prependAny<K extends keyof T>(listener: EventAndListener<K, T[K]>): this;
 	offAny<K extends keyof T>(listener: Listener<T[K]>): this;
-	removeListener<K extends keyof T>(event: string | string[], listener: Listener<T[K]>): this;
-	off<K extends keyof T>(event: string, listener: Listener<T[K]>): this;
-	removeAllListeners<K extends keyof T>(event?: string | eventNS): this;
+	removeListener<K extends keyof T>(event: K | K[], listener: Listener<T[K]>): this;
+	off<K extends keyof T>(event: K, listener: Listener<T[K]>): this;
+	removeAllListeners<K extends keyof T>(event?: K | eventNS): this;
 	setMaxListeners<K extends keyof T>(n: number): void;
 	eventNames(): string[];
-	listeners<K extends keyof T>(event: string | string[]): Listener<K>[] // TODO: not in documentation by Willian
+	listeners<K extends keyof T>(event: K | K[]): Listener<K>[] // TODO: not in documentation by Willian
 	listenersAny<K extends keyof T>(): Listener<K>[] // TODO: not in documentation by Willian
 }
